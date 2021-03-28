f2e151cec730d0d623c66cb69377a1640569090f
spinyo35 <38720867+spinyo35@users.noreply.github.com>
2021-03-29 00:55:40 +0200

    Add files via upload

---
+++ econet-utils/src/main/java/com/econetwireless/utils/enums/ResponseCode.java
@@ -0,0 +1,18 @@
+package com.econetwireless.utils.enums;
+
+/**
+ * Created by tnyamakura on 17/3/2017.
+ */
+public enum ResponseCode {
+
+    SUCCESS("200"), FAILED("500"), INVALID_REQUEST("400");
+    private String code;
+    private ResponseCode(String code) {
+        this.code = code;
+        //code= code was causing the instance variable nt to be initialised
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
