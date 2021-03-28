e4be2d5e34e4ad25ecc3e812b7b3a55aa7d3bc21
Spencer Muzipasi <smuzipasi@bdo.co.zw>
2021-03-29 00:31:14 +0200

    bug fixes specified in issues txt file

---
+++ intelligent-network-api/src/main/java/com/econetwireless/in/soap/publisher/IntelligentNetworkPublisher.java
@@ -0,0 +1,33 @@
+package com.econetwireless.in.soap.publisher;
+
+import com.econetwireless.in.soap.pool.ThreadConnectionPool;
+import com.econetwireless.in.soap.service.IntelligentNetworkServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.ws.Endpoint;
+
+/**
+ * Created by tnyamakura on 17/3/2017.
+ */
+public class IntelligentNetworkPublisher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IntelligentNetworkPublisher.class);
+
+    private IntelligentNetworkPublisher() {
+        super();
+    }
+
+    public static void  main(String ... publishingParameters) {
+        final String endpointUrl = "http://localhost:8888/intelligent-network-api/IntelligentNetworkService";
+        LOGGER.info("Starting Intelligent network service....");
+
+
+        IntelligentNetworkServiceImpl intelligentNetworkService = new IntelligentNetworkServiceImpl();
+        Endpoint endpoint =Endpoint.create(intelligentNetworkService);
+        endpoint.setExecutor(new ThreadConnectionPool());
+
+        endpoint.publish(endpointUrl);
+        LOGGER.info("Finished publishing WS");
+    }
+}
